22e1841e92606413d8e04095bc579ae21d122e33
Evans Ibok <evansibok@gmail.com>
2020-01-13 14:44:32 +0100

    setup get single user by id

--- index.js
+++ index.js
@@ -32,7 +32,22 @@ app.get('/api/users', (req, res) => {
 });
 
 app.get('/api/users/:id', (req, res) => {
+  const { id } = req.params;
 
+  findById(id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({ message: `User not found!` });
+      }
+    })
+    .catch(error => {
+      res.status(404).json({
+        message: error.message,
+        stack: error.stack,
+      });
+    })
 });
 
 app.post('/api/users', (req, res) => {
