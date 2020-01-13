cf44db82572eb762b1112e08ec7790cb69ecc9aa
Evans Ibok <evansibok@gmail.com>
2020-01-13 14:06:40 +0100

    create and spin up api server

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // implement your API here
+
+// Export Express and Cors
+const express = require('express');
+const cors = require('cors');
+
+// Instantiate Server App and Port
+const app = express();
+const port = 9000;
+
+// Plug extra functionality needed to use req and res body
+app.use(express.json());
+
+// Enable cors so app work for all origins
+app.use(cors());
+
+// Endpoints
+app.get('/', () => {
+
+});
+
+app.get('/', () => {
+
+});
+
+app.post('/', () => {
+
+});
+
+app.put('/', () => {
+
+});
+
+app.delete('/', () => {
+
+});
+
+// Start server to listen to changes
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+})
